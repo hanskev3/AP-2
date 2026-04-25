0b2e6ac749ce263ec4d2ac8e4ed07f3f5ae74a60
hanskev3 <hanshabeahan7@gmail.com>
2026-04-25 09:40:08 +0700

    Pertemuan 4 : Perulangan

---
+++ Pertemuan 4/Perulangan.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //goto label(Perloncatan)
+    //Hello world>Fasilkom-TI>Ilmu komputer>IKLC
+
+    // a: 
+    //     cout << "Hello world" << endl;
+    //     goto d;
+    // b: 
+    //     cout << "Fasilkom-TI" << endl;
+    //     return 0;
+    // c: 
+    //     cout << "Ilmu komputer" << endl;
+    //     goto b;
+    // d: 
+    //     cout << "IKLC" << endl;
+    //     goto c;
+
+    //Menampilkan bilangan genap -> 2 dengan goto
+    // int i = 10;
+    // genap:
+    // if (i % 2 == 0) {
+    //     cout<<i<<" ";
+    // } i--;
+
+    // if (i >= 2) {
+    //     goto genap;
+    // }
+
+    //Statement While = selama kondisi True maka terjadi perulangan, jika kondisi False maka berhenti.
+    // int i = 1;
+    // while (i <= 10) {
+    //     if (i % 2 == 0) {
+    //         cout<<i<<" ";
+    //     } i++;
+    // }
+
+    // Statement Do-While = perulangan yang akan ditampilkan minimal satu kali, karena kondisi akan dicek setelah perulangan pertama selesai.
+    // int i = 1;
+    // do {
+    //     cout << i << endl;
+    // } while (i <= 0);
+
+    //Statement for = perulangan yang memiliki tiga bagian yaitu inisialisasi, kondisi, dan increase.
+    // for (int i = 1; i <= 10; i+=2) {
+    //         cout<<"Hello World"<<endl;
+    // }
+
+    //nested for
+    // ***** 5 x 5
+    //  for (int i =1; i <= 5; i++) {
+    //      for (int j = 1; j <= 5; j++) {
+    //          cout<<" * ";
+    //      }
+    //      cout<<endl;
+    //  }
+
+    // Segitiga siku-siku
+    //      for (int i = 1; i <= 5; i++) {
+    //          for (int j = 1; j <= i; j++) {
+    //          cout<<" * ";
+    //      }
+    //      cout<<endl;
+    //  }
+    //           for (int i = 4; i >= 1; i--) {
+    //          for (int j = 1; j <= i; j++) {
+    //          cout<<" * ";
+    //      }
+    //      cout<<endl;
+    //  }
+}
---
+++ Pertemuan 4/latihan2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string kalimat;
+    int i;
+
+
+    cout << "Masukkan kalimat: ";
+    getline(cin, kalimat);
+
+    for (i = 0; i < kalimat.length(); i++) {
+  
+        if (kalimat[i] == 'a' ||
+            kalimat[i] == 'e' ||
+            kalimat[i] == 'i' ||
+            kalimat[i] == 'o' ||
+            kalimat[i] == 'u') {
+            kalimat[i] = toupper(kalimat[i]);
+        }
+    } 
+
+    cout << "Kalimat dengan setiap huruf vokal kapital: " << kalimat << endl;
+    return 0;
+}
